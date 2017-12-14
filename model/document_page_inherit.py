6b83f5dfcfed5c329cf5fe64771b404334941106
BrayhanJaramillo <brayhanjaramillo@gmail.com>
2017-12-13 21:52:09 -0500

    Cambios iniciales

---
+++ model/document_page_inherit.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#    
+#    Autor: Brayhan Andres Jaramillo Castaño
+#           Juan Camilo Zuluaga Serna
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import logging
+import difflib
+from openerp import models, fields, api, _
+
+_logger = logging.getLogger(__name__)
+
+
+class document_page(models.Model):
+    _name = "document.page"
+    _inherit = ['document.page']
+    _description = "Document Page inherit"
+
+    sequence_number= fields.Integer('Secuencia', required=True, readonly=True)
